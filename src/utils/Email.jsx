1f535da98a64e60ae0b87839702544c72e52aac5
hTaw99 <hady.tawfik1999@gmail.com>
2023-08-05 14:45:59 +0300

    fix 4

--- src/utils/Email.jsx
+++ src/utils/Email.jsx
@@ -14,48 +14,49 @@ import {
 // import { Tailwind } from "@react-email/tailwind";
 
 const Email = ({ name }) => (
-  <Tailwind
-  // config={{
-  //   theme: {
-  //     fontFamily: {
-  //       sans: ["inter"],
-  //     },
-  //   },
-  // }}
-  >
-    <Html>
-      <Head />
-      <Preview>
-        The sales intelligence platform that helps you uncover qualified leads.
-      </Preview>
-      <Body className="font-sans">
-        {/* <Container>
-          <Text className="font-bold text-xl">Welcome</Text>
-          <Text className="uppercase">Hi {name},</Text>
-          <Text>
-            Welcome to DukaMarket, the sales intelligence platform that helps
-            you uncover qualified leads and close deals faster.
-          </Text>
-          <Section>
-            <Button
-              pX={12}
-              pY={12}
-              className="bg-red-500 rounded-md text-white"
-              href="https://dukamarket.vercel.app"
-            >
-              Get started
-            </Button>
-          </Section>
-          <Text>
-            Best,
-            <br />
-            The DukaMarket team
-          </Text>
-          <Hr />
-        </Container> */}
-      </Body>
-    </Html>
-  </Tailwind>
+  // <Tailwind
+  // // config={{
+  // //   theme: {
+  // //     fontFamily: {
+  // //       sans: ["inter"],
+  // //     },
+  // //   },
+  // // }}
+  // >
+  <h1>Welcome</h1>
+    // <Html>
+    //   <Head />
+    //   <Preview>
+    //     The sales intelligence platform that helps you uncover qualified leads.
+    //   </Preview>
+    //   <Body className="font-sans">
+    //     {/* <Container>
+    //       <Text className="font-bold text-xl">Welcome</Text>
+    //       <Text className="uppercase">Hi {name},</Text>
+    //       <Text>
+    //         Welcome to DukaMarket, the sales intelligence platform that helps
+    //         you uncover qualified leads and close deals faster.
+    //       </Text>
+    //       <Section>
+    //         <Button
+    //           pX={12}
+    //           pY={12}
+    //           className="bg-red-500 rounded-md text-white"
+    //           href="https://dukamarket.vercel.app"
+    //         >
+    //           Get started
+    //         </Button>
+    //       </Section>
+    //       <Text>
+    //         Best,
+    //         <br />
+    //         The DukaMarket team
+    //       </Text>
+    //       <Hr />
+    //     </Container> */}
+    //   </Body>
+    // </Html>
+  // </Tailwind>
 );
 
 export default Email;
